959213ffc6b127ffb5d0382281f10da74dd02d7a
Nguyễn Ngọc Nam <n08122006@gmail.com>
2025-10-10 08:19:03 +0700

    Add files via upload

---
+++ HW1/tuanthuDIP.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP
+{
+    internal class tuanthuDIP
+    {
+        public interface IMessageSender
+        {
+            void Send(string recipient, string message);
+        }
+        public class EmailSender : IMessageSender
+        {
+            public void Send(string recipient, string message)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"[EMAIL] Gửi tới {recipient}: {message}");
+                Console.ResetColor();
+            }
+        }
+        public class SMSSender : IMessageSender
+        {
+            public void Send(string recipient, string message)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"[SMS] Gửi tới {recipient}: {message}");
+                Console.ResetColor();
+            }
+        }
+        //High-level Module (Phụ thuộc vào Abstraction)
+        public class NotificationService
+        {
+            private readonly IMessageSender _sender;
+            //tuân thủ DIP qua Dependency Injection (Constructor Injection)
+            public NotificationService(IMessageSender sender)
+            {
+                this._sender = sender;
+            }
+            public void NotifyUser(string user, string msg)
+            {
+                this._sender.Send(user, msg);
+            }
+        }
+    }
+}
---
+++ HW1/viphamDIP.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP
+{
+    internal class viphamDIP
+    {
+        // Low-level Module (Concrete Detail)
+        public class EmailSender
+        {
+            public void Send(string recipient, string message)
+            {
+                Console.WriteLine($"Gửi Email tới {recipient}: {message}");
+            }
+        }
+        // High-level Module (Business Logic)
+        public class NotificationService
+        {
+            private EmailSender _sender;
+            public NotificationService()
+            {
+                this._sender = new EmailSender();
+            }
+            public void NotifyUser(string user, string msg)
+            {
+                Console.WriteLine($"[{user}] Đang xử lý thông báo...");
+                this._sender.Send(user, msg);
+            }
+        }
+    }
+}
